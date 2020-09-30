dcf8a68f27157d3b7c7d02a203caa60296f06337
huangweiheng <huangweiheng@ezhangyu.com>
2020-09-30 15:48:19 +0800

    工具类

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+###
+ # @Author: Vhen
+ # @Date: 2020-09-30 15:45:31
+ # @LastEditors: Vhen
+ # @LastEditTime: 2020-09-30 15:47:19
+ # @Description: 
+### 
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+# npm run build
+
+# 进入生成的文件夹
+# cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m '工具类'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/Vhens/vhen-tools.git master
+
